061e50d63e68d36c2d33036833557c1c48af1c85
shanmathi1305 <rshanmathi2@gmail.com>
2024-09-25 11:49:50 -0700

    dictionary

---
+++ dict.py
@@ -0,0 +1,39 @@
+# dic={
+#     "ame":"IT",
+#     "year":2,
+#     "salary"=:30n00
+# # print(office)
+# print(type(dic))
+
+
+# school={
+#     "name":"LMS",
+#      "year":2000,
+#      "student":5000,
+#      "salary":10000
+# }
+# print(school)
+# print(type(school))
+
+# print(school["name"])
+# print["year"]=2020
+
+# print(school)
+#dic constructor:
+thisdict = dict(name = "shiva", age = 40, country = "chennai")
+# print(thisdict)
+# print(type(thisdict))
+# print(school["name"])
+# print(school.get("year"))
+# print(school.keys())
+# print(school.values())
+# school.update({"course":"maths"})
+# print(school)
+# "print(school.pop("year"))
+# print(school)
+
+thisdict.clear()
+print(thisdict)
+for i in thisdict.value():
+print(thisdict)
+
